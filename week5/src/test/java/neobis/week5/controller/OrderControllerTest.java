3a8882e10dea54f08734b942fa46d617df6dce84
Bahtiyar-Tulendybaev <119873603+Bahtiyar-Tulendybaev@users.noreply.github.com>
2024-04-15 00:14:36 +0600

    add some changes

--- week5/src/test/java/neobis/week5/controller/OrderControllerTest.java
+++ week5/src/test/java/neobis/week5/controller/OrderControllerTest.java
@@ -1,6 +1,7 @@
 package neobis.week5.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,7 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
